24c179155c97aec87e6da2761bf375077d647502
Zeyni12 <zeynebmu40@gmail.com>
2025-05-20 09:25:43 +0300

    my project

---
+++ README.md
@@ -0,0 +1,2 @@
+# My first Github Project
+This is my marketplace project. using flask
---
+++ run.py
@@ -0,0 +1,5 @@
+from market import app
+
+#checks if the rn.py file has excuted directly and not imported
+if __name__ == '__main__':
+    app.run(debug=True)
